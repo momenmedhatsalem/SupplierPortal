1bd781ebb514dade2e6d7031b8fd47205739247d
Momen Medhat <momenmedhatsalem@egmail.com>
2026-02-25 15:25:25 +0200

    fix: Remove Tailwind CSS preset import from configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,5 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  presets: [require("./node_modules/frappe-ui/src/utils/tailwind.config")],
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
