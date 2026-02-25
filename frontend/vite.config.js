0e4430adcddff862ff445dcabdab860a5080a507
Momen Medhat <momenmedhatsalem@egmail.com>
2026-02-25 15:41:07 +0200

    feat: Add unplugin-icons to devDependencies and include in Vite plugins

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Icons()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
